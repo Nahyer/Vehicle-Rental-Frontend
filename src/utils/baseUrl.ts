df97f6a4fc299ce15443f37b9092124901477d0f
nahyer <reyhanluyai@gmail.com>
2024-07-25 08:42:41 +0300

    sprint7

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,2 +1,2 @@
 // export const baseUrl = 'http://localhost:5000'
-export const baseUrl = 'https://gentle-river-05c4faf10.5.azurestaticapps.net'
+export const baseUrl = 'https://rentmyride.azurewebsites.net'
